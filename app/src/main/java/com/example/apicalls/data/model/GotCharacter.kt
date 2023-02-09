175b7b76097d621c837a366324308e7973f9afc2
lndmr <linda.koetter@gmx.de>
2023-02-09 11:40:00 +0100

    add image

--- app/src/main/java/com/example/apicalls/data/model/GotCharacter.kt
+++ app/src/main/java/com/example/apicalls/data/model/GotCharacter.kt
@@ -1,3 +1,12 @@
 package com.example.apicalls.data.model
 
-data class GotCharacter()
+data class GoTCharacter(
+    val family: String,
+    val firstName: String,
+    val fullName: String,
+    val id: Int,
+    // val image: String,
+    val imageUrl: String,
+    val lastName: String,
+    val title: String
+)
--- app/src/main/java/com/example/apicalls/data/remote/GotApiService.kt
+++ app/src/main/java/com/example/apicalls/data/remote/GotApiService.kt
@@ -1,4 +1,30 @@
 package com.example.apicalls.data.remote
 
-interface GotApiService {
+import com.example.apicalls.data.model.GoTCharacter
+import com.squareup.moshi.KotlinJsonAdapterFactory
+import com.squareup.moshi.Moshi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+
+const val BASE_URL = "https://thronesapi.com/api/v2/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface GoTApiService {
+    @GET("Characters")
+    suspend fun getCharacters(): List<GoTCharacter>
+}
+
+object GoTApi {
+    val retrofitService: GoTApiService by lazy { retrofit.create(GoTApiService::class.java) }
+
 }
